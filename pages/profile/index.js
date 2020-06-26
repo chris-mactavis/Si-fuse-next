4a49f534caf4e8490384e3c0a2294e66db8f0826
Olaide Abdulazeez <dj3plles@gmail.com>
2020-06-26 15:03:08 +0100

    Hide startup profile content
    Investors have to request for permission from startups to view their profile

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -8,7 +8,7 @@ import Profile from "../../components/Profile";
 import {User} from "../../utils/User";
 import {profileMiddleWare} from "../../components/hoc/auth";
 
-const ProfilePage = ({canView, data: {company, product_services: services, finance, market, profile, interests, connections}, userType, profileContent}) => {
+const ProfilePage = ({canView, data: {company, product_services: services, finance, market, profile, interests, level, connections}, userType, profileContent}) => {
     if (!canView) {
         Router.push('/profile/edit');
         return null;
@@ -20,7 +20,7 @@ const ProfilePage = ({canView, data: {company, product_services: services, finan
                 <title>My Profile</title>
             </Head>
 
-            <Profile company={company} services={services} finance={finance} market={market} profile={profile} interests={interests} userType={userType} hasEdit profileContent={profileContent} connections={connections} />
+            <Profile company={company} services={services} finance={finance} market={market} level={level} profile={profile} interests={interests} userType={userType} hasEdit profileContent={profileContent} connections={connections} />
         </Layout>
         <style jsx>{`
             .services-stage {
