0fe9708f66f32df0e8b79360af8e34b3415e917e
Gbolade Ogunfowote <gbolade@mactavis.com>
2020-07-03 14:11:19 +0100

    Mobile UI Fixes

--- pages/index.js
+++ pages/index.js
@@ -108,7 +108,7 @@ const Home = ({events, blogs}) => {
 
         <section id="second-section" className="image-text">
             <div className="container">
-                <div className="row mb-5">
+                <div className="row mb-5 pb-5 pb-md-0">
                     <div className="col-lg-10 col-12 mx-auto">
                         <div className="row">
                             <div className="col-lg-5 col-sm-7 col-12 mx-auto">
