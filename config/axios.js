159da2eaea3ba40a08deeeb5bcdfb525f8b66174
Gbolade Ogunfowote <gbolade@mactavis.com>
2020-07-13 16:41:36 +0100

    fixed signup width

--- config/axios.js
+++ config/axios.js
@@ -2,8 +2,8 @@ import Cookies from 'js-cookie';
 const axios = require('axios');
 console.log(Cookies.get('token'));
 const axiosInstance = axios.create({
-    baseURL: process.env.environment === 'dev' ? 'http://sifuse.test/api/' : 'http://138.68.43.103/api/',
-    //baseURL: 'http://138.68.43.103/api/',
+    // baseURL: process.env.environment === 'dev' ? 'http://sifuse.test/api/' : 'http://138.68.43.103/api/',
+    baseURL: 'http://138.68.43.103/api/',
     headers: {
         'Content-Type': 'application/json',
         // 'Authorization': 'Bearer ' + Cookies.getI
